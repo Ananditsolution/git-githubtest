277a2c7eee3e40fb3e3bbe48077d316a0edf86f5
Piyush Kumar Anand <piyushanandemail@gmail.com>
2019-11-11 16:02:35 +0530

    Second commit

---
+++ HEasyInstant_v4/src/test/java/com/dev/heasyinstant/HEasyInstantV4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.heasyinstant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HEasyInstantV4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
